e97e56da3c0cd56f71bee4aa33cc2a5c5a889497
Jennifer Hernes <jennifer@ticbits.com>
2022-12-01 09:41:31 +0200

    add solution for Day 1

---
+++ Day01/Program.cs
@@ -0,0 +1,34 @@
+﻿var input = File.ReadAllLines("./input.txt");
+
+var max1 = 0;
+var max2 = 0;
+var max3 = 0;
+var current = 0;
+foreach (var line in input)
+{
+    if (line == "")
+    {
+        if (current > max1)
+        {
+            max3 = max2;
+            max2 = max1;
+            max1 = current;
+        }
+        else if (current > max2)
+        {
+            max3 = max2;
+            max2 = current;
+        }
+        else if (current > max3)
+        {
+            max3 = current;
+        }
+        current = 0;
+    }
+    else
+    {
+        current += int.Parse(line);
+    }
+}
+
+Console.WriteLine(max1 + max2 + max3);
